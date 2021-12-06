1db3624b0d2b5d858b4afd6a7ec9ac5f76e0d39d
wisnuciwun <adiwardhanawisnu@gmail.com>
2021-12-07 00:42:12 +0700

    create main menu and routing

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,14 @@ void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: App(),
+    routes: {
+      "navbar": (context) => CustomBottomNavBar()
+    },
   ));
 }
 
 class App extends StatefulWidget {
+
   const App({Key? key}) : super(key: key);
 
   @override
@@ -16,8 +20,19 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+List<String> screens = ['Navbar'];
+
   @override
   Widget build(BuildContext context) {
-    return CustomBottomNavBar();
+    return Scaffold(
+      body: ListView.builder(itemCount: screens.length, itemBuilder: (BuildContext context, int id){
+        return Card(
+          child: ListTile(
+            title: Text(screens[id]),
+            onTap: () => Navigator.of(context).pushNamed('navbar')
+          ),
+        );
+      },),
+    );
   }
 }
